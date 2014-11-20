ca9c18f4304abd791a15a1c7b545c3805660fefd
Le Bao Linh <a1447ll@aiit.ac.jp>
2014-11-20 21:24:39 +0900

    4th commit

--- lib/mini_test_practice.rb
+++ lib/mini_test_practice.rb
@@ -4,10 +4,15 @@ module MiniTestPractice
   # Your code goes here...
   class MyClass
     def odd?(value)
-      (value % 2) == 0
+      (value % 2) == 1
     end
 
-    def check_number(i)
+    def check_number?(value)
+      (value >= 1000) and (value <= 9999) and (value % 2) == 0
+    end
+
+    def enough_length?(str)
+      (str.length >= 3) and (str.length <= 8)
     end
   end
 end
--- test/test_mini_test_practice.rb
+++ test/test_mini_test_practice.rb
@@ -17,4 +17,21 @@ class TestMiniTestPractice < MiniTest::Unit::TestCase
     assert_equal true, @my_class.odd?(1)
     assert_equal false, @my_class.odd?(2)
   end
+
+
+  def test_check_number?
+    assert_equal true, @my_class.check_number?(1000)
+    assert_equal false, @my_class.check_number?(9999)
+    assert_equal false, @my_class.check_number?(999)
+    assert_equal false, @my_class.check_number?(10000)
+  end
+
+  def test_enough_length?
+    assert_equal true, @my_class.enough_length?("rin")
+    assert_equal true, @my_class.enough_length?("12345678")
+    assert_equal false, @my_class.enough_length?("123456789")
+    assert_equal false, @my_class.enough_length?("  ")
+
+  end
+  
 end
